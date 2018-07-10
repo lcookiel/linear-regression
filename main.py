4659ece2fe4978e393310ae496f75e7a05eed957
root <eleazar.levchenko@hotmail.com>
2018-07-10 15:44:28 +0300

    Add sum of squares and some comments

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from numpy import *
 import matplotlib.pyplot as plt
 
-
+#y=mx+b
+def sse(x, y, m, b): #Sum of Squared Errors
+    sse = 0
+    for i in range(len(x)):
+        sse += (y[i]-m*x[i]-b) ** 2
+    return sse/float(len(x))
 
 def gradient_step(m, b, x, y, learning_rate):
     m_gradient = 0
     b_gradient = 0
     N = float(len(x))
     for i in range(0, len(x)):
+        #partial derrivatives
         m_gradient += -2/N * x[i] * (y[i] - m * x[i] - b)
         b_gradient += -2/N * (y[i] - m * x[i] - b)
     new_m = m - (learning_rate * m_gradient)
@@ -23,6 +29,7 @@ def gradient_descent(init_b, init_m, x, y, learning_rate, n_iterations):
     return [m, b]
 
 def run():
+    #hyperparameters
     init_m = 0
     init_b = 0
     learning_rate = 0.000001
@@ -33,8 +40,10 @@ def run():
     for i in points:
         x.append(i[0])
         y.append(i[1])
+    print('Initial values: \nm={0}, b={1}, \nlearning rate = {2}, \nn of iterations = {3}, \nsum of squared errors = {4}'.format(init_m, init_b, learning_rate, n_iterations, sse(x, y, init_m, init_b)))
+    print('Starting...')
     m, b = gradient_descent(init_b, init_m, x, y, learning_rate, n_iterations)
-    print(m, b)
+    print("After {0} iterations m = {1}, b = {2}, sum of squared errors = {3}".format(n_iterations, m, b, sse(x, y, m, b)))
     plt.plot(x, y, 'ko')
     plt.plot([0, 120], [m*0+b, m*120+b])
     plt.axis([-10, 110, -10, 120])
